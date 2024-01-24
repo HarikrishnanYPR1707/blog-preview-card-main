9108f80267de64b529a299249c08c1ae4c11fe2d
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-24 23:47:39 +0530

    Completed with the structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,27 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="h-screen border-[5px] border-black">
-      <div className="">lkajsdlkfjlaksjdf</div>
+    <div className="flex h-screen items-center justify-center border-[5px] border-black px-5">
+      <div className="border border-red-500 bg-white">
+        <div className="">
+          <img src="#" alt="svg" />
+        </div>
+        <div className="">
+          <p>Learning</p>
+          <p className="">Published 21 Dec 2023</p>
+        </div>
+        <div className="">
+          <h1 className="">HTML & CSS foundations</h1>
+          <p className="">
+            These languages are the backbone of every website, defining
+            structure, content and presentation.
+          </p>
+        </div>
+        <div className="">
+          <img src="#" alt="logo" className="" />
+          <p className="">Grep Hooper</p>
+        </div>
+      </div>
     </div>
   );
 }
